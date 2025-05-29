9fa681229303efbf0951e2b6583ddcf630abab70
jinyung0101java2 <jinyung0101java2@gmail.com>
2025-05-29 09:23:29 +0900

    Change package name

--- src/main/java/org/container/platform/web/ui/KpsPublicImageHubUiApplication.java
+++ src/main/java/org/container/platform/web/ui/KpsPublicImageHubUiApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class ContainerPlatformUiApplication {
+public class KpsPublicImageHubUiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ContainerPlatformUiApplication.class, args);
+        SpringApplication.run(KpsPublicImageHubUiApplication.class, args);
     }
 
 }
