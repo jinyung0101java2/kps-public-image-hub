ec62631125f60f5f0dd39cc69795b7a4756fea4f
jinyung0101java2 <jinyung0101java2@gmail.com>
2025-06-19 16:37:35 +0900

    Update logs

--- src/main/java/org/kps/pub/image/hub/ui/images/logs/LogsController.java
+++ src/main/java/org/kps/pub/image/hub/ui/images/logs/LogsController.java
@@ -1,52 +1,52 @@
-//package org.kps.pub.image.hub.ui.images.logs;
-//
-//import org.kps.pub.image.hub.ui.common.ConstantsUrl;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-///**
-// * Logs Controller 클래스
-// *
-// * @author hkm
-// * @version 1.0
-// * @since 2022.07.01
-// */
-//
-//@PreAuthorize("@authSecurity.checkIsGlobal()")
-//@Controller
-//public class LogsController {
-//    private static final String BASE_URL = "images/logs/";
-//
-//    /**
-//     * Logs 목록 페이지 이동(Go to the image logs list page)
-//     *
-//     * @return the view
-//     */
-//    @GetMapping(value = ConstantsUrl.URI_KPS_HUB_IMAGE_LOG)
-//    public String getImageLogsList() {
-//        return BASE_URL + "logs";
-//    }
-//
-//    /**
-//     * Logs 상세 페이지 이동(Go to the image logs details page)
-//     *
-//     * @return the view
-//     */
-//    @GetMapping(value = ConstantsUrl.URI_KPS_HUB_IMAGE_LOG + ConstantsUrl.URI_CP_DETAILS)
-//    public String getImageLogsDetail() {
-//        return BASE_URL + "logsDetail";
-//    }
-//
-//    /**
-//     * Logs 생성 페이지 이동(Go to the image logs creates page)
-//     *
-//     * @return the view
-//     */
-//    @PreAuthorize("@authSecurity.checkIsSuperAdmin()")
-//    @GetMapping(value = ConstantsUrl.URI_KPS_HUB_IMAGE_LOG + ConstantsUrl.URI_CP_CREATE)
-//    public String getImageLogsCreate() {
-//        return BASE_URL + "logsCreate";
-//    }
-//
-//}
+package org.kps.pub.image.hub.ui.images.logs;
+
+import org.kps.pub.image.hub.ui.common.ConstantsUrl;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Logs Controller 클래스
+ *
+ * @author hkm
+ * @version 1.0
+ * @since 2022.07.01
+ */
+
+@PreAuthorize("@authSecurity.checkIsGlobal()")
+@Controller
+public class LogsController {
+    private static final String BASE_URL = "images/logs/";
+
+    /**
+     * Logs 목록 페이지 이동(Go to the image logs list page)
+     *
+     * @return the view
+     */
+    @GetMapping(value = ConstantsUrl.URI_KPS_HUB_IMAGE_LOG)
+    public String getImageLogsList() {
+        return BASE_URL + "logs";
+    }
+
+    /**
+     * Logs 상세 페이지 이동(Go to the image logs details page)
+     *
+     * @return the view
+     */
+    @GetMapping(value = ConstantsUrl.URI_KPS_HUB_IMAGE_LOG + ConstantsUrl.URI_CP_DETAILS)
+    public String getImageLogsDetail() {
+        return BASE_URL + "logsDetail";
+    }
+
+    /**
+     * Logs 생성 페이지 이동(Go to the image logs creates page)
+     *
+     * @return the view
+     */
+    @PreAuthorize("@authSecurity.checkIsSuperAdmin()")
+    @GetMapping(value = ConstantsUrl.URI_KPS_HUB_IMAGE_LOG + ConstantsUrl.URI_CP_CREATE)
+    public String getImageLogsCreate() {
+        return BASE_URL + "logsCreate";
+    }
+
+}
